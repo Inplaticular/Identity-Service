a10aad05d2915df33e79c7348230c392fdf2cc79
DerMagereStudent <malo0466@gmail.com>
2022-01-31 22:32:23 +0100

    Solution and clean architecture projects created

---
+++ Src/IdentityService.WebAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Inplanticular.IdentityService.WebAPI; 
+
+public static class Program {
+	public static void Main(string[] args) {
+		var builder = WebApplication.CreateBuilder(args);
+		Program.ConfigureServices(builder);
+		var app = builder.Build();
+		Program.ConfigurePipeline(app);
+		app.Run();
+	}
+
+	public static void ConfigureServices(WebApplicationBuilder builder) {
+		// Add services to the container.
+
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+	}
+
+	public static void ConfigurePipeline(WebApplication app) {
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment()) {
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+	}
+}
