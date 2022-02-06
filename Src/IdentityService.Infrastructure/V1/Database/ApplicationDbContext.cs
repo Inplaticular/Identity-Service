d6b7580edb6dab6fb76f470ba4549dc20fc194f6
DerMagereStudent <malo0466@gmail.com>
2022-02-06 19:17:45 +0100

    INP-63 Registration - Sign up service + database context created

---
+++ Src/IdentityService.Infrastructure/V1/Database/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inplanticular.IdentityService.Infrastructure.V1.Database; 
+
+public class ApplicationDbContext : IdentityDbContext {
+	public ApplicationDbContext(DbContextOptions options) : base(options) { }
+}
---
+++ Src/IdentityService.Infrastructure/V1/Services/Authentication/SignUpService.cs
@@ -0,0 +1,39 @@
+﻿using Inplanticular.IdentityService.Core.V1.Contracts.Requests.Authentication;
+using Inplanticular.IdentityService.Core.V1.Contracts.Responses.Authentication;
+using Inplanticular.IdentityService.Core.V1.Services.Authentication;
+using Inplanticular.IdentityService.Core.V1.ValueObjects;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Inplanticular.IdentityService.Infrastructure.V1.Services.Authentication; 
+
+public class SignUpService : ISignUpService {
+	private readonly UserManager<IdentityUser> _userManager;
+
+	public SignUpService(UserManager<IdentityUser> userManager) {
+		this._userManager = userManager;
+	}
+	
+	public async Task<SignUpResponse> SignUpAsync(SignUpRequest request) {
+		var user = new IdentityUser() {
+			UserName = request.Username,
+			Email = request.Email
+		};
+
+		var creationResult = await this._userManager.CreateAsync(user, request.Password);
+		
+		if (!creationResult.Succeeded) {
+			return new SignUpResponse {
+				Errors = creationResult.Errors.Select(error => new Info() {
+					Code = error.Code,
+					Description = error.Description
+				})
+			};
+		}
+		
+		return new SignUpResponse {
+			Succeeded = true,
+			Messages = new[] {SignUpResponse.Message.SignedUp}
+		};
+	}
+}
